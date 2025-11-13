26435c4ba8a1a57350c4d44881e88ef02d7382db
gnumonik <sean@mlabs.city>
2025-11-13 02:38:32 -0500

    Have something that *might* plausibly work

--- src/Covenant/Concretify.hs
+++ src/Covenant/Concretify.hs
@@ -1,4 +1,6 @@
 {- HLINT ignore "Use if" -}
+{-# LANGUAGE ViewPatterns #-}
+
 module Covenant.Concretify (
     getTyVarConcretifications,
     getAllNodes,
@@ -34,10 +36,12 @@ import Covenant.Type (AbstractTy (BoundAt), CompT (CompN), CompTBody (ArgsAndRes
 import Control.Applicative (Alternative ((<|>)))
 import Control.Monad.Reader (Reader, runReader)
 import Covenant.DeBruijn (DeBruijn (Z), asInt)
-import Covenant.Index (Count)
+import Covenant.Index (Count, Index)
 import Data.Foldable (foldl')
-import Data.Maybe (fromJust)
+import Data.Kind (Type)
+import Data.Maybe (fromJust, mapMaybe)
 import Data.Void (Void, vacuous)
+import Data.Wedge (Wedge (Here, Nowhere, There))
 import Optics.Core (preview, review)
 
 newtype LambdaId = LambdaId {_getLamId :: Id}
@@ -50,8 +54,8 @@ newtype AppId = AppId {_getAppId :: Id}
 
    NOTE: This ONLY tells us about rigid instantiations. This is important to keep in mind,
          because we may have to perform transformations or generate "versions" of functions
-         which never
-
+         which are polymorphic but always immediately concretified by being applied to
+         concrete types.
 -}
 getTyVarConcretifications ::
     ASG ->
@@ -59,7 +63,10 @@ getTyVarConcretifications ::
     Map LambdaId (Set AppId) -> -- call sites
     Map AppId (Vector LambdaId) -> -- call site chains
     AppId ->
-    Set (Vector (ValT Void))
+    -- I think we have to return the "as concrete as possible" function type for each
+    -- call site as well. We need it in the recursive calls. If we don't have it, we can determine
+    -- THAT some type concretifies a rigid, but we can't say WHICH type that is
+    (CompT AbstractTy, Set (Map AbstractTy (ValT Void)))
 getTyVarConcretifications asg callChains callSites callSiteChains appId@(AppId i) = case nodeAt i asg of
     -- Am reasonably certain we never actually need to examine the explicit type applications/instantiations here
     AValNode _valT (App _fn _args _instTys fnTy) -> case decideConcreteCompT fnTy of
@@ -69,19 +76,44 @@ getTyVarConcretifications asg callChains callSites callSiteChains appId@(AppId i
         -}
         Nothing ->
             let rigids = collectRigids fnTy
-                resolvedRigids = combine $ resolveRigid <$> S.toList rigids
-             in S.fromList . fmap Vector.fromList $ resolvedRigids
-        Just{} ->
-            --
-            undefined
+                -- Explanation: This is a [[Map AbstractTy (ValT Void)]].
+                --              Each INNER list should be a collection of concrete singleton maps of instantiation types for a given rigid we collected from
+                --              our "as-concrete-as-possible" function type. (The maps are a convenience here, this is "morally" a [[(AbstractTy,ValT Void)]] - note the singleton-ey-ness)
+                --
+                --              The order of the outer list doesn't really indicate anything at all, it's only for grouping.
+                --
+                --              Just to make this clearer, suppose that `tN@rM` means: 'tN is a concrete type that can concretify some rigid r identified by M'
+                --              (this is a slight simplification).
+                --              resolvedRigids should look like: [ [t0@r0, t1@r0, t2@r0], [t3@r1, t4@r1], [t5@r2]  ]
+                --
+                --              What we want out of this is something like "every possible union of all of those singleton maps such that every map has
+                --              an entry for every rigid".
+                resolvedRigids = resolveRigid <$> S.toList rigids
+             in -- REVIEW: We need to tag the results with `fnTy` for recursive calls. I think? ugh
+                (fnTy,) . S.fromList . combine $ resolvedRigids
+        Just _ ->
+            -- This has to be empty. If the application site is fully concretified, there are no rigids, and there's nothing we can do.
+            (fnTy, S.empty)
     _ -> error "AppId not an app! BOOOOOM!"
   where
-    combine :: forall a. [[a]] -> [[a]]
-    combine = foldr go []
-      where
-        go :: [a] -> [[a]] -> [[a]]
-        go curr acc = (:) <$> curr <*> acc
+    -- "unsafe" way to retrieve the original (i.e. possibly polymorphic) lambda type
+    lambdaTy :: LambdaId -> CompT AbstractTy
+    lambdaTy (LambdaId l) = case nodeAt l asg of
+        ACompNode compT _ -> compT
+        _other -> error "Lambda id points at non-comp-node"
 
+    -- There has to be a term for what this does (can't remember it), but the general idea is given by the example:
+    -- `combine [["a","b"],["c"],["d","e"]]` ~ `["acd","ace","bcd","bce"]`
+    combine :: forall (a :: Type). (Monoid a) => [[a]] -> [a]
+    combine [] = []
+    combine (xs : xss) = go xs xss
+      where
+        go :: [a] -> [[a]] -> [a]
+        go as [] = as
+        go as (ys : yss) =
+            let as' = (<>) <$> as <*> ys
+             in go as' yss
+    -- This really collects every *type variable* in a CompT, but at this point, the only ones left really should be rigid.
     collectRigids :: CompT AbstractTy -> Set AbstractTy
     collectRigids =
         mconcat
@@ -98,61 +130,112 @@ getTyVarConcretifications asg callChains callSites callSiteChains appId@(AppId i
             BuiltinFlat{} -> pure S.empty
             Datatype _ args -> mconcat <$> traverse go (Vector.toList args)
 
+    -- Checks whether a "was rigid" (identified by the Index) is instantiated at a given call site of the lambda
+    -- which binds it, and if so, sorts the resulting type that instantiates it into concrete or abstract.
+    concretifies ::
+        Index "tyvar" -> -- The arg index of the rigid. We know the DeBruijn must be Z
+        AppId -> -- AppId of *a* call site of a "parent" lambda that binds the rigid we're examining
+        -- We either get:
+        --   1. Nothing (which indicates an internal error or something weird with a phantom type variable
+        --      (REVIEW: Do we sufficiently 'check' for phantom tyvars before this? The requirements here
+        --               may differ from our previous ones. A phantom tyvar is fine if it's never *used*
+        --               anywhere, but things could get really broken if we don't forbid their use
+        --               ***in explicit type applications that would result in them being a ridid***.
+        --      )
+        --   2. An rigid type variable, which means we have more work to do.
+        --   3. A concrete type, which is what we want.
+        Wedge (ValT Void) (ValT AbstractTy)
+    concretifies argPos (AppId pcsId) = case nodeAt pcsId asg of
+        AValNode _ (App fn _args _instArgs monoFunTy) ->
+            let polyFunTy = lambdaTy (LambdaId fn)
+                var = BoundAt Z argPos
+             in case instantiationHelper id var monoFunTy polyFunTy of
+                    Nothing -> Nowhere
+                    Just ty -> case decideConcrete ty of
+                        Nothing -> There ty
+                        Just ty' -> Here ty'
+        _ -> error "App node does not point at an App ValNode!!!"
+
     -- This returns EVERY POSSIBLE INSTANTIATION FOR A GIVEN ARGPOS. Or it should anyway!
     -- NOTE: That means that this is "every possibility for a specific tyvar".
-    resolveRigid :: AbstractTy -> [ValT Void]
-    resolveRigid (BoundAt db argPos) = concatMap resolveHelper parentCallSites
+    resolveRigid :: AbstractTy -> [Map AbstractTy (ValT Void)]
+    resolveRigid rigid@(BoundAt db argPos) = concatMap resolveHelper parentCallSites
       where
         -- 1. We need to find the ID of the lambda that corresponds to the DeBruijn index
-        --    of our rigid.
+        --    of our rigid (i.e. the lambda which binds that rigid)
         --    We *should* be able to get that by indexing into the entry of our
         --    call chain map for our AppId key using the DeBruijn. I think?
         --    NOTE: I'm doing unsafe indexing for convenience and to simply things, we might want to
         --          switch to safe indexing later w/ real errors
-        lambdaId = (callSiteChains M.! appId) Vector.! review asInt db
+        parentLambdaId :: LambdaId
+        parentLambdaId = (callSiteChains M.! appId) Vector.! review asInt db
         -- 2. Then we need to collect all of the call sites for that lambda. I think?
-        parentCallSites = S.toList $ callSites M.! lambdaId
-        -- 3. This is where it get complicated. For each call site, there are two possibilities:
-        --      a) It concretifies the variable we care about
-        --      b) It doesn't do that, and we have more "chasing" to do
-        --    This isn't conceptually complicated but it is tricky. We want to recurse using the top level function here
-        --    but ONLY on those call sites which do not concretify *this* variable.
-        resolveHelper :: AppId -> [ValT Void]
-        resolveHelper parent@(AppId parentAppId) = case nodeAt parentAppId asg of
-            AValNode _ (App fn _args _ concreteTy') -> case decideConcreteCompT concreteTy' of
-                -- If it's not concrete then we have more chasing to do, pretty sure.
-                -- We should be fine to call our top level function recursively here, though an unfortunate consequence of the
-                -- way I've set this up is that the only easy way to do this has HORRIBLE complexity since we'll
-                -- need to call the top level function a bunch of times. Whatever, optimize it later.
-                Nothing ->
-                    let parentVersions = S.toList $ getTyVarConcretifications asg callChains callSites callSiteChains parent
-                     in -- REVIEW: Is this right? Shouldn't we have to index into a particular position?
-                        --         But what position? The order is determined its place in the set we produce with
-                        --         `collectRigids`. Hm. Not sure what to do here. Any ideas @Koz?
-                        (Vector.! 0) <$> parentVersions
-                -- If the applied type is concrete in the parent, we need to determine what the tyvar we care about actually
-                -- got instantiated to. There isn't a *trivial* way to do that, however, fortunately we know enough to determine this:
-                -- The instantiation we're looking for will always be of the form `BoundAt Z argPos`
-                -- We do, however, need to lookup the type of the raw polymorphic function however, since we can't figure out the
-                -- instantiation without it.
-                Just concreteTy ->
-                    let var = BoundAt Z argPos
-                        polyFunTy = case nodeAt fn asg of
-                            ACompNode ty _ -> ty
-                            _ -> error "BOOOOM lambdaId points at not-a-comp-node"
-                     in case instantiationHelper var concreteTy polyFunTy of
-                            Just aResult -> case decideConcrete aResult of
-                                Nothing -> error "TODO: This really should be totally impossible. If we get this it means we swapped args around somewhere"
-                                Just concreteResult -> [concreteResult]
-                            Nothing -> error "TODO: Pretty sure this is also impossible! Only way we can get it is if we passed blatantly incorrect types to instantiationHelper"
-            _ -> error "AppId not an app! BOOOOOM!"
+        parentCallSites :: Set AppId
+        parentCallSites = callSites M.! parentLambdaId
+
+        -- 3. This is where it get complicated.
+        resolveHelper :: AppId -> [Map AbstractTy (ValT Void)]
+        resolveHelper parentAppId = case concretifies argPos parentAppId of
+            -- This happens when we can't determine the instantiation of a rigid at an application of the lambda that binds it.
+            -- We can't meaningfully recover - it means that we *need* a rigid (because it appears in the signature of a function)
+            -- but the rigid does not appear as a bound tyvar in the (applied/instantiated) function signature at a call site of its binding lambda.
+            -- (or it means we really screwed something up here!)
+            Nowhere ->
+                error $
+                    "Something went wrong when determining type variable concretifications. This could indicate either "
+                        <> " an internal compiler error, or a misuse of phantom type variables in type applications of a dependent lambda"
+            -- If it's not concrete then we have more chasing to do.
+            -- We should be fine to call our top level function recursively here, though an unfortunate consequence of the
+            -- way I've set this up is that the only easy way to do this has HORRIBLE complexity since we'll
+            -- need to call the top level function a bunch of times. Whatever, optimize it later.
+            There _abstractTy ->
+                let
+                    -- The original polymorphic function type of the lambda that binds the rigid we care about
+                    parentPolyFunTy :: CompT AbstractTy
+                    parentPolyFunTy = lambdaTy parentLambdaId
+                    -- Every transitive concretifiction of this parent lambda (the recursive call to our top level function)
+                    parentAbsFnTy :: CompT AbstractTy
+                    parentConcretifications :: [Map AbstractTy (ValT Void)]
+                    (parentAbsFnTy, parentConcretifications) = S.toList <$> getTyVarConcretifications asg callChains callSites callSiteChains parentAppId
+                    -- The concretified function types with the rigids resolved for each call site. These SHOULD REALLY BE FULLY CONCRETE.
+                    -- NOTE/REVIEW: I am not sure what to do if they are *not* fully concrete. We've kind of reached the end of the line here. There's nowhere else
+                    --              we could look, afaict, to further concretify them.
+                    fullyConcreteParentTypes :: [CompT AbstractTy]
+                    fullyConcreteParentTypes = map (\instMap -> substCompT vacuous instMap parentAbsFnTy) parentConcretifications
+                    -- We need to prove the types concrete, so we do so.
+                    -- NOTE: We might want this to error on `Nothing`s so we know that something has gone wrong
+                    --       (but I'll change that while debugging, maybe it'll just work... hahaha)
+                    provenConcreteParentTypes :: [CompT Void]
+                    provenConcreteParentTypes = mapMaybe decideConcreteCompT fullyConcreteParentTypes
+                    -- This is the var "resolved" relative to the parent lambda's level. I am pretty sure it should always be a
+                    -- `BoundAt Z ...`
+                    var = BoundAt Z argPos
+                    -- This is what we've been driving at: A list of every possible concretification of this rigid for each call site of the parent function
+                    -- NOTE(to self): might want to throw errors on `Nothing` cases instead of using `mapMaybe`, at least for debugging
+                    concretificationsForThisRigid :: [ValT Void]
+                    concretificationsForThisRigid =
+                        mapMaybe
+                            ( \concrete ->
+                                decideConcrete
+                                    =<< instantiationHelper vacuous var concrete parentPolyFunTy
+                            )
+                            provenConcreteParentTypes
+                 in
+                    M.singleton rigid <$> concretificationsForThisRigid
+            -- We already did the work in `concretifies`, we just return a singleton list of a singleton map w/ the instantiation
+            Here concreteResult -> [M.singleton rigid concreteResult]
 
 -- 'getInstantiations' but works on the types we actually need
--- NOTE: This really shouldn't ever return Nothing if it's used in the above function, maybe fromJust it?
-instantiationHelper :: AbstractTy -> CompT Void -> CompT AbstractTy -> Maybe (ValT AbstractTy)
-instantiationHelper var concrete poly = M.lookup var $ runReader (getInstantiations [var] poly' concrete') 0
+instantiationHelper ::
+    forall (a :: Type).
+    (CompT a -> CompT AbstractTy) -> -- need to pass this in so we can use this on CompT Voids
+    AbstractTy -> -- the tyvar
+    CompT a -> -- The more concrete type. Might be CompT Void if we KNOW it's concrete
+    CompT AbstractTy -> -- The more polymorphic type.
+    Maybe (ValT AbstractTy)
+instantiationHelper f var concrete poly = M.lookup var $ runReader (getInstantiations [var] poly' concrete') 0
   where
-    concrete' = Vector.toList . compTArgSchema $ vacuous concrete
+    concrete' = Vector.toList . compTArgSchema $ f concrete
     poly' = Vector.toList . compTArgSchema $ poly
 
 -- Given a list of variables, one list of types representing the function type (minus the result, we don't care about it i think),
@@ -460,3 +543,32 @@ compTBody = error "Placeholder, need a way to deconstruct a CompT that isn't a C
 compTArgSchema :: CompT a -> Vector (ValT a)
 compTArgSchema t = case compTBody t of
     ArgsAndResult args _ -> args
+
+substCompT ::
+    forall (a :: Type).
+    (ValT a -> ValT AbstractTy) ->
+    Map AbstractTy (ValT a) ->
+    CompT AbstractTy ->
+    CompT AbstractTy
+substCompT f dict (CompN cnt (compTBodyToVec -> bodyVec)) = CompN cnt (vecToCompTBody subbed) -- NOTE: COUNT WILL BE WRONG (I don't think it matters)
+  where
+    subbed = (\vt -> runReader (substitute f dict vt) 0) <$> bodyVec
+
+-- the extra function arg lets this work with either AbstractTy or Void (which might be useful for us)
+substitute ::
+    forall (a :: Type).
+    (ValT a -> ValT AbstractTy) ->
+    Map AbstractTy (ValT a) ->
+    ValT AbstractTy ->
+    Reader Int (ValT AbstractTy)
+substitute f dict = \case
+    Abstraction a ->
+        resolveVar a >>= \a' -> case M.lookup a' dict of
+            Nothing -> pure $ Abstraction a
+            Just t -> pure (f t)
+    ThunkT (CompN cnt concreteFn) -> do
+        let bodyAsVec = compTBodyToVec concreteFn
+        subbedBody <- local (+ 1) $ traverse (substitute f dict) bodyAsVec
+        pure . ThunkT . CompN cnt $ vecToCompTBody subbedBody
+    Datatype tn args -> Datatype tn <$> traverse (substitute f dict) args
+    other -> pure other
