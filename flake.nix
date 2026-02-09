bebbbad9ee55bf7637412f30b997c04c7fbaab14
Andrea Ciceri <andrea.ciceri@autistici.org>
2026-02-09 12:55:58 +0100

    Remove failing nix flake outputs

--- flake.nix
+++ flake.nix
@@ -26,7 +26,6 @@
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
 
-    herbage.url = "github:seungheonoh/herbage";
   };
 
   outputs = inputs@{ flake-parts, haskell-nix, iohk-nix, CHaP, ... }:
@@ -37,9 +36,8 @@
       ];
       debug = true;
       systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
-      hercules-ci.github-pages.branch = "staging";
 
-      perSystem = { config, system, lib, self', ... }:
+      perSystem = { config, system, ... }:
         let
           pkgs =
             import haskell-nix.inputs.nixpkgs {
@@ -48,12 +46,9 @@
                 haskell-nix.overlay
                 iohk-nix.overlays.crypto
                 iohk-nix.overlays.haskell-nix-crypto
-                inputs.herbage.overlays.default
               ];
               inherit (haskell-nix) config;
             };
-
-          herbage = inputs.herbage.lib { inherit pkgs; };
 
           project = pkgs.haskell-nix.cabalProject' {
             src = ./.;
@@ -69,10 +64,6 @@
               exactDeps = false;
               # TODO(peter-mlabs): Use `apply-refact` for repo wide refactoring `find -name '*.hs' -not -path './dist-*/*' -exec hlint -j --refactor --refactor-options="--inplace" {} +``
               shellHook = config.pre-commit.installationScript;
-              nativeBuildInputs = with pkgs; [
-                mdbook
-                hackage-repo-tool
-              ];
               tools = {
                 cabal = { };
                 haskell-language-server = { };
@@ -88,35 +79,7 @@
         in
         {
           inherit (flake) devShells;
-          hercules-ci.github-pages.settings.contents = self'.packages.combined-docs;
-          packages = flake.packages // {
-            haddock = (import ./nix/combine-haddock.nix) { inherit pkgs lib; } {
-              cabalProject = project;
-              targetPackages = [
-                "c2uplc"
-              ];
-              prologue = ''
-                = Plutarch Documentation
-                Documentation of Plutarch /and/ Documentation of Plutus libraries.
-              '';
-
-            };
-            # We do have keys for signing hackage set exposed in the repository. Once I figure out how to
-            # store secrets in Hercules CI, I'd have to fix this.
-            # However, since deployment of hackage sets are fully automatized using Hercules CI,
-            # This should remain secure as long as Plutonomicon/Plutarch repository is secure.
-            hackage =
-              herbage.genHackage
-                ./keys
-                (import ./nix/hackage.nix { inherit pkgs; });
-
-            combined-docs = pkgs.runCommand "combined-docs"
-              { } ''
-              mkdir -p $out/haddock
-              cp ${self'.packages.haddock}/share/doc/* $out/haddock -r
-              cp ${self'.packages.hackage}/* $out -r
-            '';
-          };
+          packages = flake.packages;
 
           inherit (flake) checks;
         };
